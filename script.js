4866b7a6901af23045c6870db5cf87b2f2e6fa9a
AlemDevCo <102746202+AlemDevCo@users.noreply.github.com>
2023-05-09 12:50:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ var audio = document.getElementById("myAudio");
 var playSoundBtn = document.getElementById("playSoundBtn");
 
 playSoundBtn.addEventListener("click", function() {
+	audio.currentTime = 0; // This resets the audio to the beginning before playing
 	audio.play();
 });
